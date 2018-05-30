a32d5ffb82e1c45ec3ab80f6875733908d6bb2b9
anthonylekan <anthonylekan25@gmail.com>
2018-05-30 09:30:32 -0400

    implemented smooth nav + finish welcome UI

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,6 +11,18 @@ $(document).ready(function(){
         showCursor: false
     })
 
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash,
+            $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 1200, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
 }) ;
 
 
